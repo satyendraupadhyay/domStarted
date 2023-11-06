8604fec929b5dc72909b480622f000becb9647f7
Satyendra Upadhyay <satyendraupadya49@gmail.com>
2023-11-06 19:21:32 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,22 @@
+// Examine the Document Object
+// console.dir(Document)
+
+//console.log(document.URL);
+//console.log(document.title);
+// document.title = 'Satyendra';
+// console.log(document.all);
+//console.log(document.forms);
+//console.log(document.links);
+//console.log(document.images);
+//console.log(document.head.innerText)
+//console.log(document.head.textContent)
+//console.log(document.body.innerText)
+//console.log(document.body.textContent)
+// all
+//console.log(document.all.innerText)
+//console.log(document.all.textContent)
+// console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+//console.log(headerTitle);
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px black';
