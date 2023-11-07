743153932fd4b4b16fade78f2deb7596d34cdfd2
Satyendra Upadhyay <satyendraupadya49@gmail.com>
2023-11-07 16:24:35 +0530

    3rd commit

--- dom.js
+++ dom.js
@@ -29,3 +29,5 @@ items[2].style.backgroundColor = 'green';
 for (var i  = 0;i < items.length;i++){
     items[i].style.fontWeight = 'bold';
 }
+var lastItem = document.getElementsByTagName('li');
+lastItem.style.fontWeight = 'bold';
